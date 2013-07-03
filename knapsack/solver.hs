9eca9745f746d285a4a905ba647d79b0ce6acd45
cfchou <root@wasabi.(none)>
2013-07-03 17:02:26 +0000

    hs opt list, with trace

--- knapsack/solver.hs
+++ knapsack/solver.hs
@@ -12,7 +12,7 @@ import Control.DeepSeq (($!!))
 
 import qualified Data.Vector.Unboxed as UV
 import qualified Data.List as L
-import Data.List (sortBy)
+import Data.List (sort, sortBy)
 import Data.Function (on)
 import Data.Array (Array, array)
 import qualified Data.Array as A
@@ -32,7 +32,7 @@ input = Input { file = def &= help "Data file name" &= typ "FilePath" }
 main = run . lines <$> (cmdArgs input >>= readFile . file) >>= 
        print2
 
-
+-- format output
 print2 :: [Int] -> IO () 
 print2 xs = 
     putStrLn (show (head xs) ++ " 1") >>
@@ -155,7 +155,10 @@ initRow2 k vs ws = itbl 1 $!! UV.replicate (k + 1) 0
                            if w < w_i then no_i
                            else max no_i ok_i
 
-
+-- generating a 0-1 series of size n, if a index is in xs then 1 is given,
+-- otherwise 0. 
+-- xs are item indices in ascending order, 
+to_bimap :: Int -> [Int] -> [Int]
 to_bimap n xs = reverse . snd $ foldl f (xs, []) [1..n]
     where f ([], es) i = ([], 0:es)
           f (a@(x:xs), es) i = if (i == x) then (xs, 1:es)
@@ -186,189 +189,74 @@ initRow3 k vs ws = itbl 1 $!! (V.replicate (k + 1) (0, []))
 
 
 
--- solution 4: Branch & Bound for the optimal
-ans6 :: Int -> Int -> Vector Int -> Vector Int -> [Int]
-ans6 n k vs ws = [run_bnb n k vs ws]
-
-data Bound = Bound { acc :: Int
-                   , cap :: Int
-                   , rank :: [Int]
-                   , curr :: Int
-                   } deriving (Show)
-
-ratios :: Vector Int -> Vector Int -> Vector Double
-ratios = V.zipWith (\a -> \b -> (fromIntegral a) / (fromIntegral b))
-
-sorted :: Vector Int -> Vector Int -> [Int]
-sorted vs ws = snd . unzip $ sortBy (flip (compare `on` fst)) 
-                   $ zip (V.toList $ ratios vs ws) [1..] 
-
--- better_sorted doesn't suffer from the precision problem in floats
-better_sorted :: Vector Int -> Vector Int -> [Int]
-better_sorted vs ws = 
-    let (_, _, xs) = unzip3 $ sortBy (flip f) $ 
-                        zip3 (V.toList vs) (V.toList ws) [1..]
-    in  xs
-    where f (v1, w1, _) (v2, w2, _) = compare (v1 * w2) (v2 * w1) 
-
-
-
-run_bnb :: Int -> Int -> Vector Int -> Vector Int -> Int
-run_bnb n k vs ws = 
-    let rlst = better_sorted vs ws
-        lcb = bnb vs ws n 1 True (Bound 0 k rlst 0)
-    in  bnb vs ws n 1 False (Bound 0 k rlst lcb)
-
-bnb :: Vector Int -> Vector Int -> Int -> Int -> Bool -> Bound -> Int
-bnb vs ws n i t b 
-    -- | i > n = acc b
-    | i > n = max (acc b) cb
-    | otherwise = --trace (show i ++ "  " ++ (show $ acc b) ++ ", " ++ (show $ curr b)) $
-        -- if (t && wi > cap b) then max (acc b) cb -- exceeds bound, pruned
-        if (t && wi > cap b) then cb -- exceeds bound, pruned
-        else 
-            let acc' = (acc b) + (if t then vi else 0)
-                cap' = (cap b) - (if t then wi else 0)
-                (e, c, nlst) = estimate vs ws i acc' cap' (rank b)
-                lcb = bnb vs ws n (i + 1) True (Bound acc' cap' nlst cb)
-            in  if e <= cb then cb -- estimation isn't better, pruned
-                else bnb vs ws n (i + 1) False (Bound acc' cap' nlst lcb)
-    where wi = ws ! (i - 1)
-          vi = vs ! (i - 1)
-          cb = curr b
-
-estimate :: Vector Int -> Vector Int -> Int -> Int -> Int -> [Int] 
-    -> (Int, Int, [Int])
-estimate vs ws i acc cap rlst = 
-    (\(a, b, c) -> (a, b, reverse c)) $ f acc cap [] rlst
-    where f est c newlst [] = (est, c, newlst)
-          f est c newlst xss@(x:xs) = 
-              if x == i then 
-                  f est c newlst xs
-              else 
-                  if wx > c then (est + part, 0, 
-                                  (filter (/= i) (reverse xss)) ++ newlst)
-                  else f (est + vx) (c - wx) (x:newlst) xs
-              where wx = ws ! (x - 1)
-                    vx = vs ! (x - 1)
-                    part = floor $ (fromIntegral ((vx * c) + (wx - 1))) / 
-                                      (fromIntegral wx)
-
-
--- solution 5: Branch & Bound for the optimal and selected elements
-ansBB :: Int -> Int -> Vector Int -> Vector Int -> [Int]
-ansBB n k vs ws = 
-    let (cb, cl) = run_bnb2 n k vs ws
-    -- in  cb : cl
-    in  cb : (to_bimap n cl) 
-
-data Bound2 = Bound2 { bound :: Bound
-                     , clst :: [Int]
-                     } deriving (Show)
-
-run_bnb2 :: Int -> Int -> Vector Int -> Vector Int -> (Int, [Int])
-run_bnb2 n k vs ws = 
-    let rlst = better_sorted vs ws
-        (lcb, lcl) = bnb2 vs ws n 1 True (Bound2 (Bound 0 k rlst 0) []) []
-        (rcb, rcl) = bnb2 vs ws n 1 False (Bound2 (Bound 0 k rlst lcb) lcl) []
-    in  (rcb, reverse rcl)
-
-bnb2 :: Vector Int -> Vector Int -> Int -> Int -> Bool -> Bound2 -> [Int] 
-    -> (Int, [Int])
-bnb2 vs ws n i t b2 xs
-    -- | i > n = (acc b, xs)
-    | i > n = if (acc b > cb) then (acc b, xs)
-              else (cb, cl)
-    | otherwise = --trace (show i ++ "  " ++ (show $ acc b) ++ ", " ++ (show $ curr b)) $
-        if (t && wi > cap b) then (cb, cl) -- exceeds bound, pruned
-        else 
-            let acc' = (acc b) + (if t then vi else 0)
-                cap' = (cap b) - (if t then wi else 0)
-                xs' = if t then i:xs else xs
-                (e, c, nlst) = estimate vs ws i acc' cap' (rank b) -- e >= acc'
-                (lcb, lcl) = bnb2 vs ws n (i + 1) True 
-                                (Bound2 (Bound acc' cap' nlst cb) cl) xs'
-            in  if e <= cb then (cb, cl) -- estimation isn't better, pruned
-                else bnb2 vs ws n (i + 1) False 
-                        (Bound2 (Bound acc' cap' nlst lcb) lcl) xs'
-    where wi = ws ! (i - 1)
-          vi = vs ! (i - 1)
-          b = bound b2
-          cb = curr b
-          cl = clst b2
-
 --------
 -- solution 6: Branch & Bound for the optimal and selected elements
 ans7 :: Int -> Int -> Vector Int -> Vector Int -> [Int]
-ans7 n k vs ws = [run_bnb3 n k vs ws] 
+ans7 n k vs ws = 
+    let (x:xs) = run_bnb n k vs ws
+    in  x:(to_bimap n $ sort xs)
 
-data Frac = Frac { v :: Int
-                 , w :: Int
+data Frac = Frac { fv :: Int
+                 , fw :: Int
                  , delta :: Int
                  } deriving (Show)
 
+-- cache calculated estimation
 data Est = Est { big_v :: Int
                , big_w :: Int
-               , smallAt :: Int
+               , fracAt :: Int
                , frac :: Frac
                } deriving (Show)
 
-data Bound3 = Bound3 { esti :: Est
+data Bound = Bound { esti :: Est
                      , accv :: Int
                      , accw :: Int
-                     , cap_ :: Int
-                     , curr_ :: Int
-                     , clst_ :: [Int]
+                     , curr :: Int
+                     , clst :: [Int]
                      } deriving (Show)
 
-{--
-better_sorted3 :: Vector Int -> Vector Int -> Array Int (Int, Int, Int)
-better_sorted3 vs ws = 
-    array (1, V.length vs) $ zip [1..] zipped
-    where zipped = sortBy (flip f) $ zip3 (V.toList vs) (V.toList ws) [1..]
-          f (v1, w1, _) (v2, w2, _) = compare (v1 * w2) (v2 * w1) 
---}
-
-better_sorted3 :: Vector Int -> Vector Int -> Int -> Array Int (Int, Int, Int)
-better_sorted3 vs ws k = 
+-- sortBy v/w in decending order, also drop those with weights larger than 
+-- capacity.
+better_sorted :: Vector Int -> Vector Int -> Int -> Array Int (Int, Int, Int)
+better_sorted vs ws k = 
     array (1, length zipped) $ zip [1..] zipped
-    where zipped = dropWhile (\(_, w, _) -> w > k) $ sortBy (flip f) $ zip3 (V.toList vs) (V.toList ws) [1..]
+    where zipped = dropWhile (\(_, w, _) -> w > k) $ sortBy (flip f) $ 
+                    zip3 (V.toList vs) (V.toList ws) [1..]
           f (v1, w1, _) (v2, w2, _) = compare (v1 * w2) (v2 * w1) 
  
-run_bnb3 :: Int -> Int -> Vector Int -> Vector Int -> Int
-run_bnb3 n k vs ws = 
-    let arr = better_sorted3 vs ws k
+run_bnb :: Int -> Int -> Vector Int -> Vector Int -> [Int]
+run_bnb n k vs ws = 
+    let arr = better_sorted vs ws k
         (_, n') = A.bounds arr
         est = init_est arr k
-        (lcb, lcl) = bnb3 arr n' 1 True (Bound3 est 0 0 k 0 []) []
-    in  trace (__tr n' est arr) $ fst $ bnb3 arr n' 1 False (Bound3 est 0 0 k lcb lcl) []
-    -- in  fst $ bnb3 arr n 1 False (Bound3 est 0 k lcb lcl) []
+        (lcb, lcl) = bnb3 arr n' k 1 True (Bound est 0 0 0 []) []
+        (opt, lst) = bnb3 arr n' k 1 False (Bound est 0 0 lcb lcl) []
+    in  trace (__tr n' est arr) $ opt : lst
     where __tr n est arr = ">>>>>>>>>>>>>>>>>\n" ++ show est ++ "\n" ++ 
                          show n ++ ", " ++ show k ++ 
                          "\n" ++ show arr
 
 
-bnb3 :: Array Int (Int, Int, Int) -> Int -> Int -> Bool -> Bound3 -> [Int] 
+bnb3 :: Array Int (Int, Int, Int) -> Int -> Int -> Int -> Bool -> Bound -> [Int] 
     -> (Int, [Int])
-bnb3 arr n i t b xs = trace ("> (" ++ show i ++ ", " ++ show t ++ "), (" ++ 
+bnb3 arr n k i t b xs = trace ("> (" ++ show i ++ ", " ++ show t ++ "), (" ++ 
                             show (accv b) ++ ", " ++ show (accw b) ++ ")\n" ++
-                            show (esti b)) $ bnb3' arr n i t b xs
-bnb3' arr n i t b xs
+                            show (esti b)) $ bnb3' arr n k i t b xs
+bnb3' arr n k i t b xs
     | i > n || (t && wi > k - (accw b)) = trace ("-------------") $
         if (accv b) > cb then (accv b, xs)
         else (cb, cl)
     | otherwise =
         let est' = count_est arr (esti b) k i t
-            (lcb, lcl) = bnb3 arr n (i + 1) True (Bound3 est' accv' accw' k cb cl) 
+            (lcb, lcl) = bnb3 arr n k (i + 1) True (Bound est' accv' accw' cb cl) 
                             xs'
         -- in  if (est_val est') < cb then (cb, cl)
         in  if not (est_better est' cb) then (cb, cl)
-            else bnb3 arr n (i + 1) False (Bound3 est' accv' accw' k lcb lcl) xs'
+            else bnb3 arr n k (i + 1) False (Bound est' accv' accw' lcb lcl) xs'
 
     where (vi, wi, idx) = arr A.! i
-          cb = curr_ b
-          cl = clst_ b
-          k = cap_ b
+          cb = curr b
+          cl = clst b
           accv' = (accv b) + (if t then vi else 0)
           accw' = (accw b) + (if t then wi else 0)
           xs' = if t then idx:xs else xs
@@ -380,23 +268,20 @@ est_better est cb =
     in  trace ("    " ++ show b ++ "   [[ " ++ show est ++ " >>> " ++ show cb ++ " ]]\n") b
 est_better' est cb =
     if big_v' > cb then True
-    else (v frac') * (delta frac') > (cb - big_v') * (w frac')
+    else (fv frac') * (delta frac') > (cb - big_v') * (fw frac')
     where big_v' = big_v est
           frac' = frac est
     
 
-
 count_est :: Array Int (Int, Int, Int) -> Est -> Int -> Int -> Bool -> Est
 count_est _ est _ _ True = est
-count_est arr est k i False = -- rest_est arr k est
+count_est arr est k i False =
     let (vi, wi, _) = arr A.! i 
         big_v' = (big_v est) - vi 
         big_w' = (big_w est) - wi 
-    --in  rest_est arr k (Est big_v' big_w' (smallAt est) 0)
-    -- in  rest_est arr (k - (big_w est)) $ est { big_v = big_v'
     in  rest_est arr (k - big_w') $ est { big_v = big_v'
-                                       , big_w = big_w' 
-                                       , frac = zero_frac }
+                                        , big_w = big_w' 
+                                        , frac = zero_frac }
         
 
 zero_frac = Frac 0 0 0
@@ -406,8 +291,7 @@ init_est arr k = rest_est arr k (Est 0 0 1 zero_frac)
 
 rest_est :: Array Int (Int, Int, Int) -> Int -> Est -> Est
 rest_est arr k e
-    | (smallAt e) > n = 
-        -- let e' = e { frac = zero_frac }
+    | (fracAt e) > n = 
         trace ("  | " ++ show k 
                 ++ " " ++ show e) e
     -- | k <= wi = e { small_v = floor $ (fromIntegral (vi * k + (wi - 1))) /
@@ -419,11 +303,11 @@ rest_est arr k e
     | otherwise = -- k >= wi
         let e' = e { big_v = (big_v e) + vi,
                      big_w = (big_w e) + wi,
-                     smallAt = (smallAt e) + 1 }
+                     fracAt = (fracAt e) + 1 }
         --in rest_est arr (k - wi) e'
         in rest_est arr (k - wi) $ trace ("  o " ++ show k ++ ", " ++ show wi 
                                     ++ " " ++ show e ++ "  -->  " ++ show e') e'
-    where (vi, wi, _) = arr A.! (smallAt e)
+    where (vi, wi, _) = arr A.! (fracAt e)
           n = snd $ A.bounds arr
     
 
